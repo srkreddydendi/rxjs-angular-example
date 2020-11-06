c6a78a3a0b4619aaa7eeb1730aee8a4e72065fbe
srkreddydendi <srkreddydendi@gmail.com>
2020-11-06 12:27:31 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-tevc68
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-tevc68)
---
+++ src/app/counter.actions.ts
@@ -0,0 +1,10 @@
+import { createAction } from "@ngrx/store";
+
+export const increment = createAction("[Counter Component] Increment1");
+export const decrement = createAction("[Counter Component] Decrement1");
+export const reset = createAction("[Counter Component] Reset1");
+
+/*
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://github.com/ngrx/platform
+*/
