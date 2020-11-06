b8241b67625a57098cb532c083af452bf7aab977
srkreddydendi <srkreddydendi@gmail.com>
2020-11-06 12:31:24 +0530

    1 file modified

--- src/app/counter.actions.ts
+++ src/app/counter.actions.ts
@@ -1,7 +1,7 @@
 import { createAction } from "@ngrx/store";
 
-export const increment = createAction("[Counter Component] Increment1");
-export const decrement = createAction("[Counter Component] Decrement1");
+export const increment = createAction("[Counter Component] Increment");
+export const decrement = createAction("[Counter Component] Decrement");
 export const reset = createAction("[Counter Component] Reset1");
 
 /*
